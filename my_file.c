97e3bd0a2c07c9eb6e46e18c969b39f4d0bf7171
Nagendra Bonthu <nagendrabonthu1@gmail.com>
2023-01-29 19:51:27 +0530

    MUL: Multiplication of two numbers

--- my_file.c
+++ my_file.c
@@ -4,16 +4,22 @@ int add(int a, int b)
 {
  return (a+b);
 }
+
 int sub(int a, int b)
 {
  return (a-b);
+}
+int mul(int a, int b)
+{
+ return (a*b);
 }
 void main()
 {
    int a = 2;
    int b = 5;
+
    printf("Hello world\n");
    printf("addition of %d+%d=%d\n",a, b, add(a,b));
-   printf("SUb of %d-%d=%d\n", a, b, sub(a,b));
-
+   printf("Sub of %d-%d=%d\n", a, b, sub(a,b));
+   printf("Multiplication of %dx%d=%d\n", a, b, mul(a, b));
 }
