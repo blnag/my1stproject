5beb4c10c0f46bb1f118cba007002e1d6e36e458
Nagendra Bonthu <nagendrabonthu1@gmail.com>
2023-01-29 19:49:53 +0530

    SUB: Substraction of two numbers

--- my_file.c
+++ my_file.c
@@ -4,11 +4,16 @@ int add(int a, int b)
 {
  return (a+b);
 }
+int sub(int a, int b)
+{
+ return (a-b);
+}
 void main()
 {
    int a = 2;
    int b = 5;
    printf("Hello world\n");
    printf("addition of %d+%d=%d\n",a, b, add(a,b));
+   printf("SUb of %d-%d=%d\n", a, b, sub(a,b));
 
 }
